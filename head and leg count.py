9e6ce57dfe9cceb11a5e1205eff6a5a56fe49b55
Dharmesh3108 <50350962+Dharmesh3108@users.noreply.github.com>
2019-05-13 14:33:45 +0530

    Create head and leg count.py

---
+++ head and leg count.py
@@ -0,0 +1,14 @@
+def solve(heads,legs):
+    error_msg="No solution"
+    chicken_count=0
+    rabbit_count=0
+    error_msg="No solution"
+    chicken_count=0
+    rabbit_count=0
+    if (legs%2==1 or heads>legs):
+        print(error_msg)
+    else:    
+        rabbit_count=(legs//2)-heads
+        chicken_count=heads-rabbit_count
+        print(chicken_count,rabbit_count)
+    
