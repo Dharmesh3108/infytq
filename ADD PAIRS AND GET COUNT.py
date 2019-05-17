285bdfa2fe5e470d31442256b3f1c0b25e3d888d
Dharmesh3108 <50350962+Dharmesh3108@users.noreply.github.com>
2019-05-17 11:24:45 +0530

    Create ADD PAIRS AND GET COUNT.py

---
+++ ADD PAIRS AND GET COUNT.py
@@ -0,0 +1,12 @@
+def find_pairs_of_numbers(num_list,n):
+  count=0
+  for i in range(len(num_list)):
+      for j in range(i+1,len(num_list)):
+        if(num_list[i]+num_list[j]==n):
+          count=count+1
+  print(count)        
+    #Remove pass and write your logic here
+
+num_list=[1, 2, 4, 5, 6]
+n=6
+print(find_pairs_of_numbers(num_list,n))
