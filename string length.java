a62cdd8a464a9f134dd97b93ad14277383001c49
Dharmesh3108 <50350962+Dharmesh3108@users.noreply.github.com>
2019-08-19 14:54:19 +0530

    Create string length.java

---
+++ string length.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+
+  Scanner scan=new Scanner(System.in);
+  int a=0;
+  a=scan.nextInt();
+  String arr1=""+a;
+  System.out.println(arr1.length());
+  
+  }
+}
